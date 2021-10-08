58c9730f8b00fbeda94644869274f504be0271eb
user <user@LAPTOP-OITE8GBV>
2021-10-08 12:46:01 +0800

    test

---
+++ src/test/java/testMongo/testMongo/TestMongo.java
@@ -0,0 +1,19 @@
+package testMongo.testMongo;
+
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+public class TestMongo {
+
+	public static void main(String[] args) {
+		String uri = "mongodb://127.0.0.1:27017";
+		try (MongoClient mongoClient = MongoClients.create(uri)) {
+	        MongoDatabase database = mongoClient.getDatabase("admin");
+	        MongoCollection<Document> collection = database.getCollection("system.version");
+	        Document doc = collection.find().first();
+	        System.out.println(doc.toJson());
+	    }
+}
+	}
